4593a738bb47fc1c4f6fab2393052c9f9aaf0972
TAATA EARL <fredvuni809@gmail.com>
2023-01-16 10:48:34 +0300

    remove gitignore files

--- api/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const userController = require("../controllers/Users")
-
-router.put("/:id",userController.updateUser)
-router.delete("/:id",userController.deleteUser)
-
-module.exports = router
